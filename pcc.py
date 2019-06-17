03f44881cdb1a0da3f4bab8a770e846a2f5d1bc5
csg-khu <50819420+csg-khu@users.noreply.github.com>
2019-06-17 15:16:49 +0900

    Update pcc.py

--- pcc.py
+++ pcc.py
@@ -1,22 +1,26 @@
+#This code generates Pearson correlation coefficient (PCC) and p-value from expression data. 
+#An example is shown with RNASeq expression data for cis miPs and their targets (which are shown in alternative rows in the csv file.
 from itertools import product
 import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr
-matrix =pd.read_csv('RNASeq expresion.csv', index_col=0)
-fh = open('PCC AND PVALUE.txt', 'w')
-            
-l = matrix.values.tolist()
+matrix1  =pd.read_csv('Cis miPs_RNASeq expression.csv')
+matrix2  =pd.read_csv('Cis miPs_RNASeq expression.csv', index_col=0)
+
+fh = open('miPs_PCC.txt', 'w')          
+print ("miPs\tTarget\tPCC\tp-val", file=fh)
+
+l = matrix2.values.tolist()
+
+
 i=0
 
-while i<=44616:
+while i<len(l)-1:
 
-   x=  l[i]
-   y=  l[i+1]
+
+   x=  l[i]   
+   y=  l[i+1]   
+   z=pearsonr(x,y)  
+   print(matrix1["Locus id"][i],matrix1["Locus id"][i+1],z[0], z[1],sep="\t", file=fh) 
    
-   z=pearsonr(x,y)
-
-   print>>fh, z 
    i+=2
-
-
-
